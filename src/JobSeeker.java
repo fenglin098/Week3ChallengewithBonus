abb6cf81a9f5230e9b07099c539d05e3ddcac45a
fenglin098 <feng.lin098@gmail.com>
2019-11-07 15:56:29 -0500

    first commit

--- src/JobSeeker.java
+++ src/JobSeeker.java
@@ -1,31 +1,22 @@
 public class JobSeeker {
-    private String fName;
-    private String lName;
+    private String Name;
     private String emailAddress;
+    private String phoneNumber;
 
     public JobSeeker() {
     }
 
-    public JobSeeker(String fName, String lName, String emailAddress) {
-        this.fName = fName;
-        this.lName = lName;
+    public JobSeeker(String name, String emailAddress) {
+        Name = name;
         this.emailAddress = emailAddress;
     }
 
-    public String getfName() {
-        return fName;
+    public String getName() {
+        return Name;
     }
 
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
+    public void setName(String name) {
+        Name = name;
     }
 
     public String getEmailAddress() {
@@ -34,5 +25,13 @@ public class JobSeeker {
 
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
